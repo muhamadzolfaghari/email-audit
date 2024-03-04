03a1bb670e8ef097109b2f2a1184170939844320
muhamad <muhamad.zolfaghari@gmail.com>
2024-03-04 09:06:28 +0330

    A method for get text width was added.

--- src/components/RecipientsDisplay.tsx
+++ src/components/RecipientsDisplay.tsx
@@ -1,26 +1,40 @@
-import {useEffect, useRef} from "react";
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 interface RecipientsDisplay {
   recipients: string[]
 }
 
-export default function RecipientsDisplay({recipients}: RecipientsDisplay) {
-  const rootRef = useRef<HTMLDivElement>(null);
+export default function RecipientsDisplay({
+  recipients: propsRecipients,
+}: RecipientsDisplay) {
+  const rootRef = useRef<HTMLDivElement>(null)
+  const [recipients, setRecipinets] = useState<string[]>(propsRecipients)
 
+  const getTextWidth = useCallback((text: string) => {
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')!
+    // Get font from body
+    context.font = getComputedStyle(document.body).font
+    return context.measureText(text).width
+  }, [])
 
   useEffect(() => {
+    console.log(rootRef.current!.clientWidth);
 
-    function handleResize() {
+    function handleResize() {}
 
-    }
-
-    document.addEventListener("resize", handleResize);
+    document.addEventListener('resize', handleResize)
 
     return () => {
-      document.removeEventListener("resize", handleResize);
+      document.removeEventListener('resize', handleResize)
     }
-  }, []);
+  }, [])
 
-
-  return <div ref={rootRef}>{recipients.map(recipinet => <div>{recipinet}</div>)}</div>
+  return (
+    <div ref={rootRef}>
+      {recipients.map(recipinet => (
+        <div>{recipinet}</div>
+      ))}
+    </div>
+  )
 }
